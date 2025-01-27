8cac01bf9d14ec449fe43410b8754da2409bdf67
lrndwy <hafiztamvan15@gmail.com>
2025-01-27 15:04:03 +0700

    ahahah

--- core/settings.py
+++ core/settings.py
@@ -135,6 +135,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_DIRS = [BASE_DIR / 'static']
+
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = '/media/'
